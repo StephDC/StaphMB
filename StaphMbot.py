4e02fc4e8520cd465b9a705dc1ec04a5e25a22e5
Staph. aureus <staph@aosc.io>
2018-05-26 13:09:33 -0400

    Add check for warn history
    No one should be punished for the same speech twice.

--- StaphMbot.py
+++ StaphMbot.py
@@ -5,6 +5,7 @@ import urllib.error as ue
 import urllib.request as ur
 import urllib.parse as up
 import json
+import datetime
 import time
 import sys
 import os
@@ -35,7 +36,7 @@ class tgapi:
             raise APIError('API', 'Initialization Self-test Failed')
         print("Bot "+self.info["username"]+" connected to the Telegram API.")
 
-    def query(self,met,parameter=None):
+    def query(self,met,parameter=None,retry=None):
         req = ur.Request(self.target+met,method='POST')
         req.add_header('User-Agent','StaphMbot/0.1 (+https://github.com/StephDC/StaphMbot)')
         if parameter is not None:
@@ -43,15 +44,16 @@ class tgapi:
             req.data = json.dumps(parameter).encode('UTF-8')
         #            print(req.data.decode('UTF-8'))
         retryCount = 0
+        maxRetry = retry if retry is not None else self.retry
         failed = True
         while failed:
             try:
                 resp = ur.urlopen(req)
             except ue.HTTPError:
-                if retryCount == self.retry:
+                if retryCount >= maxRetry:
                     raise APIError('API','Query HTTP Error')
             except ue.URLError:
-                if retryCount == self.retry:
+                if retryCount >= maxRetry:
                     raise APIError('API','Query DNS Error')
             else:
                 failed = False
@@ -99,6 +101,15 @@ def randomID():
 def countWarn(db,gid,uid):
     return db[2].data.execute('select count(header) from warn where user=? and "group"=?',(str(uid),str(gid))).fetchone()[0]
 
+def getName(uid,gid,api,lookup={}):
+    if uid in lookup:
+        return '@'+lookup[uid]
+    try:
+        result = api.query('getChatMember',{'chat_id':int(gid),'user_id':int(uid)},retry=1)
+    except APIError:
+        return 'a former admin of this group'
+    return '@'+result['user']['username']
+
 def processItem(message,db,api):
     print(message['update_id'],'being processed...')
     if 'message' not in message:
@@ -117,11 +128,14 @@ def processItem(message,db,api):
                 if message['message']['chat']['type']!='supergroup':
                     api.sendMessage(message['message']['chat']['id'],'抱歉，警告功能僅在超級群組有效。',{'reply_to_message_id':message['message']['message_id']})
                 else:
-                    adminList = [i['user']['id'] for i in api.query('getChatAdministrators',{'chat_id':message['message']['chat']['id']})]
+                    adminList = {i['user']['id']:i['user']['username'] for i in api.query('getChatAdministrators',{'chat_id':message['message']['chat']['id']})}
                     if message['message']['from']['id'] not in adminList:
                         api.sendMessage(message['message']['chat']['id'],'抱歉，僅有濫權管理員方可使用 #WARN 警告其他用戶。',{'reply_to_message_id':message['message']['message_id']})
                     elif 'reply_to_message' not in message['message']:
                         api.sendMessage(message['message']['chat']['id'],'用法錯誤：請回覆需要被警告的訊息。',{'reply_to_message_id':message['message']['message_id']})
+                    elif db[2].data.execute('SELECT count(header) from warn where "group"=? and "text"=?',(str(message['message']['chat']['id']),str(message['message']['reply_to_message']['message_id']))).fetchone()[0]:
+                        warnInfo = db[2].data.execute('SELECT time,admin,reason from warn where "group"=? and "text"=?',(str(message['message']['chat']['id']),str(message['message']['reply_to_message']['message_id']))).fetchone()
+                        api.sendMessage(message['message']['chat']['id'],'抱歉，該條訊息已於 '+datetime.datetime.fromtimestamp(int(warnInfo[0])).isoformat()+' 被 '+getName(warnInfo[1],message['message']['chat']['id'],api,adminList)+' 以理由「 '+warnInfo[2]+' 」警告過。',{'reply_to_message_id':message['message']['message_id']})
                     else:
                         warnInfo = [int(time.time()),message['message']['chat']['id'],message['message']['reply_to_message']['from']['id'],message['message']['reply_to_message']['message_id'],message['message']['from']['id'],message['message']['text'][5:].strip()]
                         if not warnInfo[-1]:
@@ -131,13 +145,13 @@ def processItem(message,db,api):
                         elif warnInfo[2] == api.info['id'] or message['message']['reply_to_message']['from']['is_bot']:
                             api.sendMessage(message['message']['chat']['id'],'竟敢試圖警告機器人，你的請求被濫權掉了。',{'reply_to_message_id':message['message']['message_id']})
                         else:
-                            notAdded = True
-                            while notAdded:
-                                db[2].addItem([randomID()]+warnInfo)
-                                api.sendMessage(message['message']['chat']['id'],'警告成功。該用戶現在共有 '+str(countWarn(db,warnInfo[1],warnInfo[2]))+' 個警告。',{'reply_to_message_id':message['message']['message_id']})
-                             #   else:
-                                print('Warned '+message['message']['reply_to_message']['from']['username']+' in group '+message['message']['chat']['title'])
-                                notAdded = False
+                            notUnique = True
+                            while notUnique:
+                                id = [randomID()]
+                                notUnique = db[2].hasItem(id[0])
+                            db[2].addItem(id+warnInfo)
+                            api.sendMessage(message['message']['chat']['id'],'警告成功。該用戶現在共有 '+str(countWarn(db,warnInfo[1],warnInfo[2]))+' 個警告。',{'reply_to_message_id':message['message']['message_id']})
+                            print('Warned '+message['message']['reply_to_message']['from']['username']+' in group '+message['message']['chat']['title'])
     elif 'new_chat_participant' in message['message']:
         if message['message']['new_chat_participant']['id'] == api.info["id"]:
             addGroup(message['message']['chat']['id'],db)
